4a3b1358b96cf1073f4ba9cb63268f8e297f8404
xiyang.jia <xiyang.jia@phigent.ai>
2024-03-11 14:46:25 +0800

    update

--- cpp/design_pattern/singleton/test.cpp
+++ cpp/design_pattern/singleton/test.cpp
@@ -12,11 +12,13 @@ public:
   std::string getName() { return "Singleton"; };
 
 private:
-  Singleton() {}
-  Singleton(const Singleton &other) {}
+  Singleton() { std::cout << "Deconstruct Singleton" << std::endl; }
+
   ~Singleton() noexcept {
+    std::cout << "Deconstruct Singleton" << std::endl;
   } // noexcept关键字告诉编译器，函数中不会发生异常,这有利于编译器对程序做更多的优化。如果在运行时，noexecpt函数向外抛出了异常（如果函数内部捕捉了异常并完成处理，这种情况不算抛出异常），程序会直接终止，调用std::terminate()函数，该函数内部会调用std::abort()终止程序。
-  Singleton &operator=(const Singleton &other) { return *this; }
+  Singleton(const Singleton &other) = delete;
+  Singleton &operator=(const Singleton &other) = delete;
 };
 
 // 恶汉版：main之前初始化
@@ -27,10 +29,11 @@ public:
   std::string getName() { return "Singleton1"; };
 
 private:
-  Singleton1() {}
-  Singleton1(const Singleton1 &other) {}
-  ~Singleton1() noexcept {}
-  Singleton1 &operator=(const Singleton1 &other) { return *this; }
+  Singleton1() { std::cout << "Construct Singleton1" << std::endl; }
+
+  ~Singleton1() noexcept { std::cout << "Deconstruct Singleton1" << std::endl; }
+  Singleton1(const Singleton1 &other) = delete;
+  Singleton1 &operator=(const Singleton1 &other) = delete;
 
 private:
   static Singleton1 singleton_;
